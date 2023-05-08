102eaa806b310777fbd0bc5868f18e37b01f2c12
Zohaib Hassan <zohaibiqbal3800@gmail.com>
2023-05-08 19:37:17 +0500

    Navbar and Hero

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter } from "react-router-dom";
+import Contact from "./components/Contact";
+import Loader from "./components/Loader";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Feedback from "./components/Feedbacks";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Stars from "./components/canvas/Stars";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <div className="relative z-0">
+          {/* <Contact />
+        <Stars/> */}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
