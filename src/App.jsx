e247611d4aabe919e3b9ece66ea04bf198d9aa18
Zohaib Hassan <zohaibiqbal3800@gmail.com>
2023-05-11 04:10:44 +0500

    Testimonials Section

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ const App = () => {
           <Experience />
           <Tech />
           <Works />
+          <Feedback />
         </div>
         <div className="relative z-0">
           <Contact />
